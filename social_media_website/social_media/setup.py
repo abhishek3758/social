d4f7047db96b65ccf94ea5c223d8d613a89b3636
Abhishek <abhishekdingla4882@gmail.com>
2020-05-12 01:02:18 +0530

    secondcommit

---
+++ social_media_website/social_media/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='social',
+    version='1.0',
+    packages=[],
+    url='/social/',
+    license='',
+    author='abhishek',
+    author_email='singlaabhishek84@gmail.com',
+    description='Hospital management system'
+)
